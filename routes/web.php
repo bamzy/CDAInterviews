dd99f492df081ab6785897726bed994eb2171bf8
Bamdad <aghilide@ualberta.ca>
2020-06-03 12:05:39 -0400

    auto added laravel auth module

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,16 @@ Route::get('/contacts', function () {
     return view('contacts');
 });
 
-Route::get('page/{id}', 'PageController@render');
+Route::get('page/{id}', 'PageController@render')->middleware('auth');;
 Route::post('page/update/', 'PageController@store');
 Route::post('item/update/', 'ItemController@store');
+
+
+// Authentication routes...
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');
+Route::get('auth/logout', 'Auth\AuthController@getLogout');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
