a3b14dcfb069fd5b95b4b84da2f10d82f88955cd
Bamdad <aghilide@ualberta.ca>
2020-06-03 20:05:29 -0400

    some clean ups

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -8,69 +8,39 @@
 
     @if ($page->indexed) <meta name="robots" content="noindex"> @endif
 
-<!-- Styles -->
 
-    <meta name="google-site-verification" content="XHAz5PWm2RHaIVuVJQ-gwi54Wn5O-LVlXL7QCExSSzA">
-    <meta name="online" content="true">
-    <meta name="viewport" content="initial-scale=1 maximum-scale=1">
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+
     <meta name="description" content="FREE Ultimate Guide to CDA Interviews: Tips &amp; Proven Strategies to Help You Prepare &amp; Ace Your CDA Structured Interview. ">
 
     <title>FREE Ultimate Guide to CDA Interviews: Tips &amp; Proven Strategies to Help You Prepare &amp; Ace Your CDA Structured Interview. </title>
-    <link rel="stylesheet" type="text/css" media="screen" href="http://cdainterview.com/rw_common/themes/Endeavor/styles.css">
-    <link rel="stylesheet" type="text/css" media="screen" href="http://cdainterview.com/rw_common/themes/Endeavor/styles.css">
-{{--    <link rel="stylesheet" type="text/css" media="screen" href="http://cdainterview.com/rw_common/themes/Endeavor/colourtag-page0.css">--}}
-    <link rel="stylesheet" type="text/css" media="screen" href="http://cdainterview.com/rw_common/themes/Endeavor/flexslider.css">
-    <link rel="stylesheet" type="text/css" media="screen" href="http://cdainterview.com/rw_common/themes/Endeavor/css/contentcenter.css">
-    <link rel="stylesheet" type="text/css" media="screen" href="http://cdainterview.com/rw_common/themes/Endeavor/css/ec9on.css">
-    <link rel="stylesheet" type="text/css" media="screen" href="http://cdainterview.com/rw_common/themes/Endeavor/css/rimage.css">
-    <link rel="stylesheet" type="text/css" media="screen" href="http://cdainterview.com/rw_common/themes/Endeavor/css/ssoff.css">
-    <link rel="stylesheet" type="text/css" media="screen" href="http://cdainterview.com/rw_common/themes/Endeavor/css/sslide.css">
-    <link rel="stylesheet" type="text/css" media="screen" href="http://cdainterview.com/rw_common/themes/Endeavor/css/sidenone.css">
-    <link rel="stylesheet" type="text/css" media="screen" href="http://cdainterview.com/rw_common/themes/Endeavor/css/olight90.css">
-    <link rel="stylesheet" type="text/css" media="screen" href="http://cdainterview.com/rw_common/themes/Endeavor/css/fontarial.css">
-    <link rel="stylesheet" type="text/css" media="screen" href="http://cdainterview.com/rw_common/themes/Endeavor/css/title26.css">
-    <link rel="stylesheet" type="text/css" media="screen" href="http://cdainterview.com/rw_common/themes/Endeavor/css/fontarialspan.css">
-    <link rel="stylesheet" type="text/css" media="screen" href="http://cdainterview.com/rw_common/themes/Endeavor/css/bts46.css">
-    <link rel="stylesheet" type="text/css" media="screen" href="http://cdainterview.com/rw_common/themes/Endeavor/css/bt60.css">
-    <link rel="stylesheet" type="text/css" media="screen" href="http://cdainterview.com/rw_common/themes/Endeavor/css/fontarialnav.css">
-    <link rel="stylesheet" type="text/css" media="screen" href="http://cdainterview.com/rw_common/themes/Endeavor/css/nav17.css">
-    <link rel="stylesheet" type="text/css" media="screen" href="http://cdainterview.com/rw_common/themes/Endeavor/css/fontarialside.css">
-    <link rel="stylesheet" type="text/css" media="screen" href="http://cdainterview.com/rw_common/themes/Endeavor/css/fontarialheader.css">
-    <link rel="stylesheet" type="text/css" media="screen" href="http://cdainterview.com/rw_common/themes/Endeavor/css/fontarialcontent.css">
-    <link rel="stylesheet" type="text/css" media="screen" href="http://cdainterview.com/rw_common/themes/Endeavor/css/font13.css">
 
     <style type="text/css" media="all">#feature {background-image: url(http://cdainterview.com/resources/cda-interview-guide.jpg);}</style>
     <link re="stylesheet" type="text/css" media="screen" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.css">
-    <!--[if lt IE 9]>
-    <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-    <script type="text/javascript" src="http://cdainterview.com/rw_common/themes/Endeavor/scripts/html5shiv.js"></script>
-    <![endif]-->
 
-    <script type="text/javascript" src="http://cdainterview.com/rw_common/themes/Endeavor/scripts/jquery-1.7.1.min.js"></script>
-
+    <script
+        src="https://code.jquery.com/jquery-3.5.1.min.js"
+        integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
+        crossorigin="anonymous"></script>
 
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
 
 
 
 
-    <link rel="stylesheet" type="text/css" media="all" href="http://cdainterview.com/rw_common/plugins/stacks/stacks.css">
-    <!--[if lte IE 7]>
-    <link rel='stylesheet' type='text/css' media='all' href='http://cdainterview.com/rw_common/plugins/stacks/stacks_ie.css' />
-    <![endif]-->
-    <link rel="stylesheet" type="text/css" media="all" href="http://cdainterview.com/index_files/stacks_page_page0.css">
-    <script type="text/javascript" charset="utf-8" src="http://cdainterview.com/index_files/stacks_page_page0.js"></script>
+{{--    <link rel="stylesheet" type="text/css" media="all" href="http://cdainterview.com/rw_common/plugins/stacks/stacks.css">--}}
+{{--    <!--[if lte IE 7]>--}}
+{{--    <link rel='stylesheet' type='text/css' media='all' href='http://cdainterview.com/rw_common/plugins/stacks/stacks_ie.css' />--}}
+{{--    <![endif]-->--}}
+{{--    <link rel="stylesheet" type="text/css" media="all" href="http://cdainterview.com/index_files/stacks_page_page0.css">--}}
+{{--    <script type="text/javascript" charset="utf-8" src="http://cdainterview.com/index_files/stacks_page_page0.js"></script>--}}
 
 
     <meta name="{{$page->meta_name}}" content="{{$page->meta_description}}">
     <link rel="stylesheet" href="{{ URL::asset('css/simple-grid.min.css') }}" />
+    <link rel="stylesheet" href="{{ URL::asset('css/styles.css') }}" />
     <link rel="stylesheet" href="{{ URL::asset('css/mystyle.css') }}" />
     <noscript><img height="1" width="1" alt="" style="display:none" src="https://www.facebook.com/tr?id=235586069975455&amp;ev=NoScript" /></noscript><!-- End Google Analytics -->
 
